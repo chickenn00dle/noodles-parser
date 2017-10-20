a6afec6aa671ce210cf6c954b388523f8b47488b
Glitch (narrow-alligator) <none>
2017-10-20 08:33:34 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const port = process.env.PORT;
 const app = express();
 
 app.get('/', (req, res) => {
-  let ipAddress = req.get('x-forwarded-for').replace(/^(.*),.*/, '$1'),
-      language = req.get('accept-language'),
-      software = req.get('user-agent');
+  let ipAddress = req.get('x-forwarded-for').replace(/^([0-9.]*),.*/, '$1'),
+      language = req.get('accept-language').replace(/^([a-zA-Z-]*),.*/, '$1'),
+      software = req.get('user-agent').replace(/^.*\s\((.*)\).*/, '$1');
+
+  console.log(software);
   
-  console.log(ipAddress);
+  //Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36
   
   let json = {
     'ipaddress': ipAddress,
