b26455cc64aa401185e696e75f354d0a1f973357
Glitch (narrow-alligator) <none>
2017-10-20 04:42:09 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,47 +4,16 @@ const port = process.env.PORT;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(req);
+  console.log(process.env);
+  let ipAdd = pr
 });
 
-const listener = app.listen(port, (err, data) => {
+const listener = app.listen(port, (err, event) => {
   if (err) throw err;
-  console.log('Listening on port ' + listener.address().port);
+  console.log('Listening on port ' + port);
 });
 
-
-
-// const express = require('express');
-// const moment = require('moment');
-
-// const app = express();
-
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/views/index.html');
-// });
-
-// app.get('/:date', (req, res) => {
-//   if (req.params) {
-//     let param = req.params.date,
-//         unix = null,
-//         natural = null;
-    
-//     if (moment.unix(param).isValid()){
-//       unix = param;
-//       natural = moment.unix(param).format('MMMM DD, YYYY');
-//     } else if (moment(param).isValid()) {
-//       unix = moment(param).format('X');
-//       natural = param;
-//     }
-    
-//     let json = {
-//       "unix": unix,
-//       "natural": natural
-//     }
-    
-//     res.send(json);
-    
-//   }
+// const listener = app.listen(process.env.PORT, (err, res) => {
+//   if (err) throw err;
+//   console.log('Your app is listening on port ' + listener.address().port);
 // });
